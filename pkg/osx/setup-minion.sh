f2a0b0cb44767f69ededbb5a9904163dfd2cb527
Nahum Shalman <nshalman-github@elys.com>
2016-03-02 17:04:40 -0500

    binary installer

--- pkg/osx/setup-minion.sh
+++ pkg/osx/setup-minion.sh
@@ -1,17 +1,33 @@
 #!/bin/bash -x
 
-[[ -z ${1} ]] && echo "Please provide name of salt master as an argument" && exit 1
+MINION=/etc/salt/minion
+
+update_master () {
+	sed -e '/^master:/d' -i .bak ${MINION}
+	echo "master: ${1}" >> ${MINION}
+}
+
 
 THIS="$(cd $(dirname ${0}); pwd)/$(basename ${0})"
+unzip -u "${THIS}"
 
 [[ $USER == root ]] || exec sudo -H ${THIS} "$@"
 
-mkdir -p /etc/salt
-echo "master: ${1}" > /etc/salt/minion
+if grep -q "^master:" ${MINION}
+then
+	echo "Found existing master name in ${MINION}"
+	[[ ! -z ${1} ]] && echo "Updating ${MINION} with ${1} as new master" && update_master ${1}
 
-cd /
-unzip -f "${THIS}"
-sudo installer -pkg ./salt-*.pkg -target /
+else
+	[[ -z ${1} ]] && echo "Please provide name of salt master as an argument" && exit 1
+	mkdir -p /etc/salt
+	touch ${MINION}
+	update_master ${1}
+fi
+
+installer -pkg ./salt-*.pkg -target /
 cp ./start-salt-minion.sh /opt/salt/bin/start-salt-minion.sh
+chmod +x /opt/salt/bin/start-salt-minion.sh
+[[ -z ${2} ]] && rm -f salt-*.pkg start-salt-minion.sh
 sudo launchctl kickstart -k system/com.saltstack.salt.minion
-exit $?
+exit 0
--- pkg/osx/start-salt-minion.sh
+++ pkg/osx/start-salt-minion.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+START_SALT="exec /opt/salt/bin/salt-minion $@"
+
 SERIAL_NUMBER=$(python -c 'import plistlib,subprocess; print plistlib.readPlistFromString(subprocess.check_output("system_profiler SPHardwareDataType -xml",shell=True))[0]["_items"][0]["serial_number"]')
 MINION_ID_STRING="id: mac-${SERIAL_NUMBER}"
 
 # if minion id is correct, start salt
-grep -q "^${MINION_ID_STRING}$" /etc/salt/minion && exec /usr/local/salt/bin/salt-minion
+grep -q "^${MINION_ID_STRING}$" /etc/salt/minion && ${START_SALT}
 
 #otherwise, purge pki keys, fix minion config, then start salt
 rm -rf /etc/salt/pki
-mv /etc/salt/minion{,.bak}
-sed -e '/^id/d' /etc/salt/minion.bak > /etc/salt/minion
+sed -e '/^id/d' -i .bak /etc/salt/minion
 echo "${MINION_ID_STRING}" >> /etc/salt/minion
-exec /opt/salt/bin/salt-minion $@
+${START_SALT}
